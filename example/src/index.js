b89c28f4f28f7ff7469afe772d3e6f15afc91c6a
Winter Zhong <wmzhong@users.noreply.github.com>
2018-04-23 14:10:47 +0800

    添加例子

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Mask from '../../src/Mask'
 ReactDOM.render((
-        <Mask />
+        <Mask autoClose={true} />
     ),
     document.getElementById('app')
 )
